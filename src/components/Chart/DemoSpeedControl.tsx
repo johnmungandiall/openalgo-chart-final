892a739467e76983bba060c2ace7b88ca6c56114
John <johnmungandistartups@gmail.com>
2026-05-01 13:51:16 +0530

    feat: demo speed 50x added

--- src/components/Chart/DemoSpeedControl.tsx
+++ src/components/Chart/DemoSpeedControl.tsx
@@ -9,7 +9,7 @@ interface DemoSpeedControlProps {
     onSpeedChange: (speed: number) => void;
 }
 
-const SPEED_OPTIONS = [1, 2, 3, 5, 8, 10];
+const SPEED_OPTIONS = [1, 2, 3, 5, 8, 10, 20, 50];
 
 const DemoSpeedControl: React.FC<DemoSpeedControlProps> = ({ speed, onSpeedChange }) => {
     const [collapsed, setCollapsed] = useState(false);
